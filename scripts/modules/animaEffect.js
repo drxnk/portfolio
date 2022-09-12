4ba3f6b00674617d30ae3995b287ebea9f2fbf0e
Luiz Felipe <luizfelipecode@hotmail.com>
2022-09-11 22:06:44 -0300

    concertando animacoes contato

--- scripts/modules/animaEffect.js
+++ scripts/modules/animaEffect.js
@@ -1,9 +1,13 @@
 export default function initAnimaEffect() {
   const targetData = document.querySelectorAll("[data-anima]");
+  const contatoHand = document.querySelector("[data-animat='right-hand']");
   const animationClass = "animate";
+  const animationClassHand = "animate-hand";
 
   function animationScroll() {
     const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+
+    
     targetData.forEach(function (el) {
       if (windowTop > el.offsetTop) {
         el.classList.add(animationClass);
@@ -11,6 +15,12 @@ export default function initAnimaEffect() {
         el.classList.remove(animationClass);
       }
     });
+
+    if(windowTop > contatoHand.offsetTop) {
+      contatoHand.classList.add(animationClassHand)
+    } else {
+      contatoHand.classList.remove(animationClassHand)
+    }
   }
 
   animationScroll();
